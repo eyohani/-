6bc62253e6d2461885634a24ec4583e79516f41c
eyohani <144806351+eyohani@users.noreply.github.com>
2024-04-11 02:34:01 +0900

    to do list
    to do list

---
+++ yohan.js
@@ -0,0 +1,25 @@
+// script.js
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("할 일을 입력해야 합니다!");
+    } else {
+      document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+  
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+    span.onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+  
