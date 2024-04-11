57e87d0949cc828087998893a785dbf8a25ee1bb
eyohani <144806351+eyohani@users.noreply.github.com>
2024-04-11 14:20:19 +0900

    Add files via upload

--- yohan.js
+++ yohan.js
@@ -1,25 +1,44 @@
-// script.js
+document.getElementById("myInput").addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    newElement();
+  }
+});
+
 function newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-      alert("할 일을 입력해야 합니다!");
-    } else {
-      document.getElementById("myUL").appendChild(li);
-    }
-    document.getElementById("myInput").value = "";
-  
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-  
-    span.onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
+  var inputValue = document.getElementById("myInput").value;
+  if (inputValue === '') {
+    alert("입력을 하세요!!!");
+    return;
   }
   
+  var li = document.createElement("li");
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+
+  var editBtn = document.createElement("BUTTON");
+  editBtn.innerHTML = "수정";
+  editBtn.className = "editBtn";
+  li.appendChild(editBtn);
+
+  var deleteBtn = document.createElement("BUTTON");
+  deleteBtn.innerHTML = "삭제";
+  deleteBtn.className = "deleteBtn";
+  li.appendChild(deleteBtn);
+
+  deleteBtn.onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+
+  editBtn.onclick = function() {
+    var newText = prompt("수정할 내용을 입력하세요", inputValue);
+    if (newText !== null && newText !== '') {
+      t.nodeValue = newText;
+    }
+  }
+
+  var ul = document.getElementById("myUL");
+  ul.appendChild(li);
+
+  document.getElementById("myInput").value = "";
+}
